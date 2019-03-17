5d66999a4de67a4bc3b14d9b6468a421b5856d70
falko <falko@lehmann-carpzov.de>
2019-03-17 15:16:05 +0100

    convertet code into class, added Color, Brightness Support
    Initial commit

---
+++ boot.py
@@ -0,0 +1,37 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+#import uos, machine
+#uos.dupterm(None, 1) # disable REPL on UART(0)
+
+
+#boot.py
+import gc
+import network
+import ubinascii
+import webrepl
+
+print('[boot]')
+
+SSID = '<fillme>'
+password = '<fillme>'
+
+wlan = network.WLAN(network.STA_IF) # STA_IF means station interface
+ap = network.WLAN(network.AP_IF)    # AP_IF means access point
+print('Shutting down access point...')
+ap.active(False)    # shut down access point
+
+if not wlan.isconnected():
+    print('Connecting to wifi network...')
+    wlan.active(True)
+    wlan.connect(SSID, password)
+    while not wlan.isconnected():
+        pass
+
+(ip, netmask, gateway, dns) = wlan.ifconfig()
+
+webrepl.start()
+gc.collect()
+
+print('[boot] - DONE')
+
--- dev.py
+++ dev.py
@@ -3,66 +3,88 @@ import neopixel
 from umqtt.simple import MQTTClient
 import ubinascii
 
-SERVER="192.168.1.4"
-USER="mqtt_user"
-PASS="mqtt_pass"
-SUB_TOPIC="pixelbrett/cmd/#"
-CLIENT_ID = ubinascii.hexlify(machine.unique_id())
-ANNOUNCE_TOPIC="homeassistant/light/pixelbrett/config"
+class HomeAssistantMQTTLight:
+
+    def __init__(self):
+        self.SERVER="192.168.1.4"
+        self.USER="mqtt_user"
+        self.PASS="mqtt_pass"
+        self.SUB_TOPIC="pixelbrett/cmd/#"
+        self.CLIENT_ID = ubinascii.hexlify(machine.unique_id())
+        self.ANNOUNCE_TOPIC="homeassistant/light/pixelbrett/config"
+
+        self.brightness=100
+        self.c_red=100
+        self.c_green=100
+        self.c_blue=100
+
+        self.mqtt_c = None
+        self.pixels = neopixel.NeoPixel(machine.Pin(4), 5*19)
+
+    def all_pixel(self,r,g,b):
+        f = self.brightness/255
+        for i in range(0, self.pixels.n):
+            self.pixels[i] = (int(r*f),int(g*f),int(b*f))
+        self.pixels.write()
+
+    def mqtt_cb(self,topic, msg):
+        msg=str(msg,"ANSII")
+        print((topic, msg))
+
+        if "brightness" in topic:
+            self.brightness=int(msg)
+            self.all_pixel(self.c_red, self.c_green, self.c_blue)
+            self.updateState("brightness", self.brightness)
+        if "color" in topic:
+            r, g, b = str(msg).split(',')
+            self.c_red = int(r)
+            self.c_green = int(g)
+            self.c_blue = int(b)
+            self.all_pixel(self.c_red, self.c_green, self.c_blue)
+            self.updateState("color", msg) #cheated with the color string
+        if msg == "ON":
+            self.all_pixel(self.c_red, self.c_green, self.c_blue)
+            self.updateState("light", msg)
+        elif msg == "OFF":
+            self.all_pixel(0,0,0)
+            self.updateState("light", msg)
+
+    def updateState(self, type, msg):
+        self.mqtt_c.publish("pixelbrett/state/"+type, str(msg))
+
+    def start_sub(self):
+        self.mqtt_c.subscribe(self.SUB_TOPIC)
+        print("Connected to %s, subscribed to %s topic" % (self.SERVER, self.SUB_TOPIC))
+        try:
+            while 1:
+                self.mqtt_c.wait_msg()
+        finally:
+            self.mqtt_c.disconnect()
 
 
-def all_pixel(r,g,b):
-    for i in range(0, pixels.n):
-        pixels[i] = (r,g,b)
-    pixels.write()
+    def init_mqtt(self):
+        self.mqtt_c = MQTTClient(client_id=self.CLIENT_ID, server=self.SERVER, user=self.USER, password=self.PASS)
+        self.mqtt_c.set_callback(self.mqtt_cb)
+        self.mqtt_c.connect()
 
-def mqtt_cb(topic, msg):
-    print((topic, msg))
-    if msg == b"ON":
-        all_pixel(0,100,0)
-        updateState("light", msg)
-    elif msg == b"OFF":
-        all_pixel(0,0,0)
-        updateState("light", msg)
+    def publish_device(self):
+        msg = """
+        {
+            "name":"pixelbrett",
+            "device_class": "light",
+            "state_topic": "pixelbrett/state/light",
+            "command_topic": "pixelbrett/cmd/light",
+            "brightness_command_topic": "pixelbrett/cmd/brightness",
+            "brightness_state_topic": "pixelbrett/state/brightness",
+            "brightness_scale": 100,
+            "rgb_command_topic": "pixelbrett/cmd/color",
+            "rgb_state_topic": "pixelbrett/state/color"
+        }
+        """
+        self.mqtt_c.publish(self.ANNOUNCE_TOPIC,msg)
 
-def updateState(type, msg):
-    mqtt_c.publish("pixelbrett/state/"+type,msg)
-
-def start_mqtt_sub(c):
-    c.subscribe(SUB_TOPIC)
-    print("Connected to %s, subscribed to %s topic" % (SERVER, SUB_TOPIC))
-    try:
-        while 1:
-            c.wait_msg()
-    finally:
-        c.disconnect()
-
-
-def mqtt_init():
-    c = MQTTClient(client_id=CLIENT_ID, server=SERVER, user=USER, password=PASS)
-    c.set_callback(mqtt_cb)
-    c.connect()
-    return c
-
-def mqtt_publish_device(c):
-    msg = """
-    {
-        "name":"pixelbrett",
-        "device_class": "light",
-        "state_topic": "pixelbrett/state/light",
-        "command_topic": "pixelbrett/cmd/light",
-        "brightness_command_topic": "pixelbrett/cmd/brightness",
-        "brightness_state_topic": "pixelbrett/state/brightness"
-    }
-    """
-    c.publish(ANNOUNCE_TOPIC,msg)
-
-
-
-#ransom Text to make file bigger!!!!
-
-pixels = neopixel.NeoPixel(machine.Pin(4), 5*19)
-all_pixel(20,20,20);
-mqtt_c = mqtt_init()
-mqtt_publish_device(mqtt_c)
-start_mqtt_sub(mqtt_c)
+hawl = HomeAssistantMQTTLight()
+hawl.all_pixel(20,20,20);
+hawl.init_mqtt()
+hawl.publish_device()
+#hawl.start_mqtt_sub()
